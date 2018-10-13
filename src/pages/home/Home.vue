ebc73447b8f4ed859e417ef96e0de9d996c9de0f
qiuliming <858752692@qq.com>
2018-10-13 22:49:09 +0800

    add other depens

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>home</div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'Home',
+  component: {
+    Header
+  }
+}
+</script>
+
+<style>
+</style>

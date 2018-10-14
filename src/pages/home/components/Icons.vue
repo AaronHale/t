3384e64594eea3d8f408f795cc4c70621ff13b7c
qiuliming <858752692@qq.com>
2018-10-14 12:10:25 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="icons">
-    <div class="icon" v-for="item in iconsList" :key="item.id">
-      <div class="icon-img">
-        <img class="icon-img-content" :src="item.imgUrl" alt="">
-      </div>
-      <p class="icon-desc">{{item.desc}}</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div
+          class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class='icon-img'>
+            <img class='icon-img-content' :src='item.imgUrl' />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 <script>
@@ -51,45 +59,63 @@ export default {
           imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
           desc: '一日游'
         }
-      ]
+      ],
+      swiperOption: {
+        autoplay: false
+      }
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconsList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import "~styles/variable.styl"
+  @import '~styles/variable.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
   .icons
-    width 100%
-    height 0
-    padding-bottom 50%
-    overflow hidden
+    margin-top: .1rem
     .icon
-      width 25%
-      float left
-      padding-bottom 25%
-      overflow hidden
-      position relative
-      height 0
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
       .icon-img
-        box-sizing border-box
-        padding .1rem
-        position absolute
-        top 0
-        left 0
-        right 0
-        bottom .44rem
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
         .icon-img-content
-          display block
-          margin 0 auto
-          height  100%
+          display: block
+          margin: 0 auto
+          height: 100%
       .icon-desc
-        position absolute
-        left 0
-        right 0
-        bottom 0
-        line-height .44rem
-        height .44rem
-        color $darkTextColor
-        text-align center
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
 </style>
